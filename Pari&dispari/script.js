2dd62348dfe9aef1c61ce5184dec71128592f8be
roberto-palanga <r.palanga94@gmail.com>
2024-02-06 17:31:55 +0100

    inserimento funzioni

--- Pari&dispari/script.js
+++ Pari&dispari/script.js
@@ -1,19 +1,43 @@
 
-// L’utente sceglie pari o dispari.
+// L’utente sceglie pari o dispari. V
 
-// Inserisce un numero da 1 a 5.
+// Inserisce un numero da 1 a 5. V
 
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). V
 
-// Sommiamo i due numeri
+// Sommiamo i due numeri V
 
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
 // Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
 
+function pcNumber (){
+
+    let randomNumber = Math.floor(Math.random()* 5) + 1;
+    
+
+    return randomNumber;
+
+}
+
+function add (a,b){
+     
+    let sum = a + b;
+    
+    return sum
+}
+
+
 
 let userChoice; 
-let eRRor = false;
+console.log("scelta dell'utente"+ userChoice)
+
+let pcChoice = pcNumber();
+console.log("Scelta computer" + pcChoice)
+
+
+let DorP ;
+let errorCicle = true;
 
 do {
 
@@ -25,28 +49,43 @@ do {
 
     } else {
 
-        eRRor = true;
+        errorCicle = false;
         
 
     }
 
     
     
-} while (eRRor !== true )
+} while (errorCicle !== false )
+
+let sumElement = add(pcChoice, userChoice);
+console.log( "questa è la somma" + sumElement)
 
 
+/*
+!A && !B  ====  !(A || B)
+!A || !B  ==== !(A && B)
+//while  (DorP !== "pari" && DorP !== "dispari"))  {
+*/
 
-function pcNumber (){
+while  (!(DorP === "pari" || DorP === "dispari"))  {
 
-    let randomNumber = Math.floor(Math.random()* 5) + 1;
-    
+    DorP = prompt("Inserisci Pari o Dispari") ;
 
-    return randomNumber;
+    if (!(DorP === "pari" || DorP === "dispari")){
+        alert("inserisci la parola pari o dispari")
+    }
 
 }
 
+console.log(DorP)
 
-let pcChoice = pcNumber()
+
+
+
+
+
+
 
 // console.log(pcChoice)
 
