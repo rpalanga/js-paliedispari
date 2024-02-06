0c8895f361a59e5728a05449f99fc86b93372902
roberto-palanga <r.palanga94@gmail.com>
2024-02-06 18:07:18 +0100

    Inserimento parte finale dell'esercizio

--- Pari&dispari/script.js
+++ Pari&dispari/script.js
@@ -7,7 +7,7 @@
 
 // Sommiamo i due numeri V
 
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) V
 
 // Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
 
@@ -28,14 +28,32 @@ function add (a,b){
 }
 
 
+function check (sumElement) {
 
+    if (sumElement % 2 == 0){
+        return "pari"
+        console.log("il numero è pari")
+
+    } else {
+        return "dispari"
+        console.log("il numero è dispari")
+
+    }
+}
+
+
+// scelta utente
 let userChoice; 
 console.log("scelta dell'utente"+ userChoice)
 
+// scelta randomica computer
 let pcChoice = pcNumber();
 console.log("Scelta computer" + pcChoice)
 
+// valutere se la somma degli elementi è pari o dispari
+let Result = check (sumElement);
 
+// pari o dispari
 let DorP ;
 let errorCicle = true;
 
@@ -58,15 +76,12 @@ do {
     
 } while (errorCicle !== false )
 
+// somma degli elementi 
 let sumElement = add(pcChoice, userChoice);
 console.log( "questa è la somma" + sumElement)
 
 
-/*
-!A && !B  ====  !(A || B)
-!A || !B  ==== !(A && B)
-//while  (DorP !== "pari" && DorP !== "dispari"))  {
-*/
+// controllo sulle parole inserite
 
 while  (!(DorP === "pari" || DorP === "dispari"))  {
 
@@ -87,7 +102,15 @@ console.log(DorP)
 
 
 
-// console.log(pcChoice)
+
+
+
+
+
+
+
+
+
 
 
 
